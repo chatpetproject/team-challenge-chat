79b4822bab81398ebd4d8a220cc19323c92d7f00
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-22 15:53:04 +0300

    Fix Main Title component

--- src/UI/MainTitle.ts
+++ src/UI/MainTitle.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const MainTitle = styled.h1`
-  color: ${(props) => props.theme.colors.main};
+  color: ${(props) => props.theme.colors.blackColor[0]};
 `;
