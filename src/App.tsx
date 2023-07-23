cd241bee5770cccd8d4daf276e4c992cad517131
Serhii.Staryk <serhii.staryk@gmail.com>
2023-07-23 23:37:24 +0300

    add styled button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
-import { MainTitle } from './UI/MainTitle';
+import { Button } from './UI/Button';
+import { MainTitle } from './UI/Text';
 
 function App() {
-  return <MainTitle>Team challenge chat</MainTitle>;
+  return (
+    <>
+      <MainTitle>Buttons</MainTitle>
+
+      <Button variant='primary' width='262px' height='45px'>
+        Enter to the chat
+      </Button>
+
+      <Button variant='secondary' width='262px' height='45px'>
+        Enter to the chat
+      </Button>
+    </>
+  );
 }
 export default App;
---
+++ src/UI/Button.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { ButtonProps } from './Button.d';
+
+export const Button = styled.button<ButtonProps>`
+  border: 0;
+  margin: 10px;
+  outline: none;
+  cursor: pointer;
+  font-size: 24px;
+  font-weight: 500;
+  border-radius: 10px;
+  transition: all 0.2s linear;
+  color: ${(props) => props.theme.colors.whiteColor};
+
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+
+  ${(props) =>
+    props.variant === 'primary' &&
+    `
+    border: 3px;
+    background: ${props.theme.gradients.main};
+    `}
+
+  &:hover {
+    ${(props) => props.variant === 'primary' && `border: solid #fff;`}
+  }
+
+  ${(props) =>
+    props.variant === 'secondary' &&
+    `
+    background: transparent;
+    border: 1px solid ${props.theme.colors.whiteColor};
+    `}
+
+  &:hover {
+    ${(props) =>
+      props.variant === 'secondary' &&
+      `
+        border: 0;
+        color: ${props.theme.colors.blackColor[1]};
+        background: ${props.theme.colors.whiteColor};
+    `}
+  }
+
+  &:active {
+    opacity: 0.7;
+  }
+`;
