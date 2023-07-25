2f5b3c3c586544a5af0e0e51e331974d0dea5f34
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-25 13:33:13 +0300

    Add logo interface

--- src/UI/Logo.ts
+++ src/UI/Logo.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { LogoProps } from './Logo.d';
 
-export const Logo = styled.img`
+export const Logo = styled.img<LogoProps>`
   width: 78px;
   height: 39px;
 `;
