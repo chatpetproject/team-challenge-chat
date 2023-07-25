ae741b4ebf87769854c05e2d62cee49f8aeaf463
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-25 13:12:56 +0300

    Add LanSelect and Logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { Button } from './UI/Button';
 import { MainTitle } from './UI/Text';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <MainTitle>Buttons</MainTitle>
-
-      <Button variant='primary' width='262px' height='45px'>
-        Enter to the chat
-      </Button>
-
-      <Button variant='secondary' width='262px' height='45px'>
-        Enter to the chat
-      </Button>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,2 +1,20 @@
-function Header() {}
+import { Button } from '../UI/Button';
+import { LanSelect } from '../UI/LanSelect';
+import { Logo } from '../UI/Logo';
+import LogoImage from '../assets/logo.svg';
+
+function Header() {
+  return (
+    <>
+      <Logo src={LogoImage}></Logo>
+      <LanSelect>English</LanSelect>
+      <Button variant='secondary' width='157px' height='45px'>
+        Sign up
+      </Button>
+      <Button variant='primary' width='262px' height='45px'>
+        Enter to the chat
+      </Button>
+    </>
+  );
+}
 export default Header;
