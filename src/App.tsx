23719301c6745b4afb4019e1fde415de7fead0b6
Serhii Staryk <56160276+SerhiiStaryk@users.noreply.github.com>
2023-08-03 00:18:22 +0300

    Route layout (#9)
    * fix close button
    
    * add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import { Button } from './UI/Button';
-import { MainTitle } from './UI/Text';
-import Modal from './components/Modal/Modal';
-import useModal from './hooks/useModal';
+import Layout from './Layout';
 
 function App() {
-  const { isOpen, openModal, closeModal } = useModal();
   return (
     <>
-      <MainTitle>Buttons</MainTitle>
-
-      <Button variant='primary' width='262px' height='45px'>
-        Enter to the chat
-      </Button>
-
-      <Button variant='secondary' width='262px' height='45px'>
-        Enter to the chat
-      </Button>
-
-      <Button
-        width='262px'
-        height='45px'
-        variant='secondary'
-        onClick={openModal}
-      >
-        Open Modal
-      </Button>
-      <Modal
-        isOpen={isOpen}
-        onClose={closeModal}
-        titleText='Sign up to start chatting'
-      >
-        {''}
-      </Modal>
+      {/* <Header /> */}
+      <Layout />
+      {/* <Footer /> */}
     </>
   );
 }
