8cdef826c20be9f2126b80c0cb018cef1aaf754f
Sofia Ruban <sophiaruban249324@gmail.com>
2023-08-07 14:35:30 +0300

    Delete LanSelect.ts

--- src/UI/LanSelect.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import ArrowDownImg from '../assets/arrow-down.svg';
-
-export const LanSelect = styled.div`
-  background: right url(${ArrowDownImg}) no-repeat;
-  width: 110px;
-  height: 30px;
-  margin: 15px 48px;
-`;
