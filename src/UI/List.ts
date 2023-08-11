0efbaa3bb8c0ebe8c731c8b87156ba5ca199e5cb
Serhii Staryk <56160276+SerhiiStaryk@users.noreply.github.com>
2023-08-11 09:34:14 +0300

    Home page (#12)
    * fix prettier
    
    * fix footer
    
    * create home page
    
    * fix code

---
+++ src/UI/List.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  gap: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+`;
+
+export const ListItem = styled.li``;
